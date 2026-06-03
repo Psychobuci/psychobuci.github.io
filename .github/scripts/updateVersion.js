ae95219f910fa5ee5c8b48142701e78ff9fe84d5
Psychobuci <psychobuci@gmail.com>
2026-06-03 10:10:17 +0200

    updateVersion fix

--- .github/scripts/updateVersion.js
+++ .github/scripts/updateVersion.js
@@ -1,7 +1,7 @@
 const admin = require("firebase-admin");
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-const appUrl = JSON.parse(process.env.FIREBASE_APP_URL);
+const appUrl = process.env.FIREBASE_APP_URL;
 
 admin.initializeApp({
   credential:
